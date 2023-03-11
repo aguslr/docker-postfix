c8c144a3adebfc550fcd2492f4415fbc3e79a9c4
Agus Lopez <contact@aguslr.com>
2023-03-11 13:31:33 +0100

    Add Dockerfile for Postfix
    Add Dockerfile to build a container running *Postfix*.
    
    https://www.postfix.org/
    https://wiki.archlinux.org/title/Postfix
    https://github.com/juanluisbaptiste/docker-postfix

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+ARG BASE_IMAGE=alpine:latest
+
+FROM docker.io/${BASE_IMAGE}
+
+RUN \
+  apk add --update --no-cache postfix cyrus-sasl-login \
+  && rm -rf /var/cache/apk/*
+
+COPY entrypoint.sh /entrypoint.sh
+
+EXPOSE 25/tcp
+
+HEALTHCHECK --interval=1m --timeout=3s \
+  CMD timeout 2 nc -z 127.0.0.1 25
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["start-fg"]
---
+++ entrypoint.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Configure Postfix
+postconf -e 'maillog_file = /dev/stdout'
+postconf -e 'inet_interfaces = all'
+if env | grep -q ^POSTFIX_; then
+
+	postconf -e "myhostname = ${POSTFIX_HOSTNAME}"
+	if [ "${POSTFIX_DOMAIN}" ]; then
+		postconf -e "mydomain = ${POSTFIX_DOMAIN}"
+	else
+		postconf -e "mydomain = ${POSTFIX_HOSTNAME#*.}"
+	fi
+	postconf -e 'myorigin = $mydomain'
+
+	if [ "${POSTFIX_DESTINATION}" ]; then
+		postconf -e "mydestination = ${POSTFIX_DESTINATION}"
+	else
+		postconf -e 'mydestination = $myhostname, localhost.$mydomain, localhost, $mydomain'
+	fi
+
+	if [ "${POSTFIX_NETWORKS}" ]; then
+		postconf -e "mynetworks = ${POSTFIX_NETWORKS}"
+	else
+		postconf -e 'mynetworks = 127.0.0.0/8, 192.168.0.0/16'
+	fi
+
+	postconf -e "relayhost = [${POSTFIX_RELAYSERVER}]:${POSTFIX_RELAYPORT:-587}"
+	if [ "${POSTFIX_RELAYUSER}" ]; then
+		postconf -e 'smtp_sasl_auth_enable = yes'
+		postconf -e 'smtp_sasl_password_maps = lmdb:/etc/postfix/sasl_passwd'
+		postconf -e 'smtp_sasl_security_options = noanonymous'
+		postconf -e 'smtp_tls_wrappermode = yes'
+		postconf -e 'smtp_tls_security_level = encrypt'
+		postconf -e 'smtp_use_tls = yes'
+		if [ ! -f /etc/postfix/sasl_passwd ]; then
+			printf '[%s]:%d %s:%s\n' \
+				"${POSTFIX_RELAYSERVER}" "${POSTFIX_RELAYPORT:-587}" \
+				"${POSTFIX_RELAYUSER}" "${POSTFIX_RELAYPASS}" \
+				> /etc/postfix/sasl_passwd
+		fi
+		postmap lmdb:/etc/postfix/sasl_passwd
+	fi
+
+	if [ "${POSTFIX_OVERWRITE}" ]; then
+		printf '/^From:.*$/ REPLACE From: %s\n' "${POSTFIX_OVERWRITE}" \
+			> /etc/postfix/smtp_header_checks
+		postmap /etc/postfix/smtp_header_checks
+		postconf -e 'smtp_header_checks = regexp:/etc/postfix/smtp_header_checks'
+	fi
+
+	if [ "${POSTFIX_FROM}" ]; then
+		printf '/^(.*)@.*\.%s$/ %s\n' "${POSTFIX_DOMAIN}" "${POSTFIX_FROM}" \
+			> /etc/postfix/canonical_sender
+		postconf -e 'sender_canonical_classes = envelope_sender, header_sender'
+		postconf -e 'sender_canonical_maps = regexp:/etc/postfix/canonical_sender'
+	fi
+
+	if [ "${POSTFIX_TO}" ]; then
+		printf '/^.*@.*\.%s$/ %s\n' "${POSTFIX_DOMAIN}" "${POSTFIX_TO}" \
+			> /etc/postfix/canonical_recipient
+		postconf -e 'recipient_canonical_classes = envelope_recipient, header_recipient'
+		postconf -e 'recipient_canonical_maps = regexp:/etc/postfix/canonical_recipient'
+	fi
+
+fi
+
+# Start Postfix
+/usr/sbin/postfix "$@"
